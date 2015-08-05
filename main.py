af60812313ff456d1707ac53763bb590cbaa9598
Kseniia <kseniia.pinigina@gmail.com>
2015-08-05 17:10:30 -0400

    Change post parameters

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
 import time
 import random
+import json
 
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
 
 @app.route('/get_data', methods=['POST'])
 def add_task():
@@ -13,13 +18,19 @@ def add_task():
     if request.method == 'POST':
         client_url = request.args.get('client_url', '')
         asins = request.args.get('products_asins', '')
-
+	if not client_url or not asins:
+	    client_url = request.form.get('client_url', '')
+            asins = request.form.get('products_asins', '')
+	    if not client_url or not asins:    
+	        return 'Not all parameters'
         hash = random.getrandbits(128)
         file_name = '/tmp/'+str(hash)+'.json'
+	
+	product_asins = {"asins": asins}	
 
-        command = "scrapy crawl amazon -a client_url='"+client_url+\
-                  "' -a product_asins='"+asins+"' -a file_name='"+file_name+"'"
-
+        command = "cd /home/ubuntu/amazonmatchingreview/ && scrapy crawl amazon -a client_url='"+client_url+\
+                  "' -a product_asins='"+json.dumps(product_asins)+"' -a file_name='"+file_name+"'"
+	print command, '*'*40
         os.system(command)
         result = ''
         while True:
