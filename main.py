865630416b07ba92d80190f3bd969b41f454595e
Kseniia <kseniia.pinigina@gmail.com>
2015-08-11 19:03:32 -0400

    Change main.py file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 import random
 import json
+import re
 
 from flask import Flask
 from flask import request
@@ -16,13 +17,43 @@ def hello():
 def add_task():
     print 'add_task'
     if request.method == 'POST':
+        try:
+#	    print '!'*100
+#            print request.is_json
+#            print dir(request)
+            print request.data
+	    print request.form
+            print type(request.data)
+#            print request.json
+#            j = request.get_json(force=True)
+#            print j
+        except Exception as e:
+            print e
         client_url = request.args.get('client_url', '')
         asins = request.args.get('products_asins', '')
 	if not client_url or not asins:
 	    client_url = request.form.get('client_url', '')
             asins = request.form.get('products_asins', '')
-	    if not client_url or not asins:    
-	        return 'Not all parameters'
+	    if not client_url or not asins:
+                try:
+                    data = json.loads(request.data)
+                    client_url = data['client_url']
+                    asins = data['products_asins']
+                except Exception as e:
+                    print e
+                if not client_url or not asins:
+	            return 'Not all parameters'
+
+        regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        if not re.match(regex, client_url):
+            return "Sorry, we receive incorect url '%s', please check it" % client_url
+
         hash = random.getrandbits(128)
         file_name = '/tmp/'+str(hash)+'.json'
 	
@@ -31,7 +62,10 @@ def add_task():
         command = "cd /home/ubuntu/amazonmatchingreview/ && scrapy crawl amazon -a client_url='"+client_url+\
                   "' -a product_asins='"+json.dumps(product_asins)+"' -a file_name='"+file_name+"'"
 	print command, '*'*40
-        os.system(command)
+	try:
+            os.system(command)
+	except:
+	    return 'Invalid parametrs, please check it'
         result = ''
         while True:
             try:
